874bec7c9ea5cef88240aab0ace33bc0c45903cd
kirang89 <kiran.daredevil@gmail.com>
2013-08-19 14:09:29 +0530

    Added home url and changed admin credentials

--- djangoboilerplate/urls.py
+++ djangoboilerplate/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'djangoboilerplate.views.home', name='home'),
+    url(r'^$', 'djangoboilerplate.views.home', name='home'),
     # url(r'^djangoboilerplate/', include('djangoboilerplate.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- scripts/createadmin.py
+++ scripts/createadmin.py
@@ -2,8 +2,8 @@
 
 from django.contrib.auth.models import User
 if User.objects.count() == 0:
-    admin = User.objects.create(username='admin')
-    admin.set_password('admin')
+    admin = User.objects.create(username='kiran')
+    admin.set_password('walletkit')
     admin.is_superuser = True
     admin.is_staff = True
     admin.save()
